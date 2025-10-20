5b929eab404ba75c771cd7abca3faa7a90c99492
GaboC2006 <ga.castillol@duocuc.cl>
2025-10-19 21:09:37 -0300

    Pruebas Unitarias V1

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,18 @@
+/// <reference types="vitest" />
+
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+
+  test: {
+
+    environment: 'jsdom',       // importante: configura entorno tipo navegador
+    globals: true,              // para usar expect, test, etc. sin importar
+    setupFiles: './src/tests/setup.ts', // archivo para configuraciones globales
+    css: true,
+  }
 })
+
